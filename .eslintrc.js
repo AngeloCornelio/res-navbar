c1753d87d11784d0e8d9b0ff1e1b73b9e8b832d0
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-08-11 10:50:16 -0700

    Change ESLint to AirBnB standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,14 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 12
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
